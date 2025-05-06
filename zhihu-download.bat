ee1af8ba76f4a523debc3991540646bab600ec99
GdUniverse <842812163@qq.com>
2025-05-06 16:14:16 +0800

    在bat中增加了看到app.log输出的功能(共web界面进入使用)

--- zhihu-download.bat
+++ zhihu-download.bat
@@ -1,1 +1,9 @@
-D:\conda\envs\zhihu2Mark\python.exe app.py
+@echo off
+start D:\conda\envs\zhihu2Mark\python.exe app.py
+if not exist ".\logs\app.log" (
+    echo 日志文件不存在，请检查路径
+    pause
+    exit /b
+)
+powershell -command "Get-Content '.\logs\app.log' -Encoding UTF8 | Select-Object -Last 4"
+pause
