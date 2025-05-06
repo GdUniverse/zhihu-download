ac88c64be8eda5c21c614d67ee10d9760ccefd09
GdUniverse <842812163@qq.com>
2025-05-06 23:41:09 +0800

    之前的方法太不稳定，由于处理速度不定倒数第3行不一定能对上，直接访问本地接口

--- zhihu-download.bat
+++ zhihu-download.bat
@@ -1,8 +1,3 @@
 @echo off
-start D:\conda\envs\zhihu2Mark\python.exe app.py
-if not exist ".\logs\app.log" (
-    echo 日志文件不存在，请检查路径
-    pause
-    exit /b
-)
-powershell -command "$urlLine = Get-Content '.\logs\app.log' -Encoding UTF8 | Select-Object -Last 3 | Select-Object -First 1; if ($urlLine -match 'https?://[^\s]+') { start $matches[0] }"
+start http://127.0.0.1:5000
+D:\conda\envs\zhihu2Mark\python.exe app.py
